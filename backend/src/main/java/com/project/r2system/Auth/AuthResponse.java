253b212b5932a4f3a10a8be6a273f14e3bac7447
lino62U <alupoc@unsa.edu.pe>
2023-09-07 09:30:06 -0500

    Authorizations

---
+++ backend/src/main/java/com/project/r2system/Auth/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.project.r2system.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token; 
+}
