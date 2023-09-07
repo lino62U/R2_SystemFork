366572f064f05e6bc679ee9acab2a01e232b8eeb
unknown <ldavis@unsa.edu.pe>
2023-09-06 19:58:55 -0500

    f

--- frontend/src/pages/Error404.js
+++ frontend/src/pages/Error404.js
@@ -1,7 +1,20 @@
+import Cookies from 'js-cookie'
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 export default function Error404() {
-  return (
-    <div>Pagina no encontrada</div>
-  )
+  const token = Cookies.get("token")
+
+  // Mensaje de error
+  const errorMessage = "Pagina no encontrada";
+
+  if (token) {
+    // Si hay un token, muestra el mensaje y redirige a "/"
+    alert(errorMessage);
+    return <Navigate to="/" />;
+  }
+
+  // Si no hay token, muestra el mensaje y redirige a "/home"
+  alert(errorMessage);
+  return <Navigate to="/home" />;
 }
