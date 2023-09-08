fbcbc943d5dac7a32d54978288044dc3193c14f7
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2023-09-08 14:13:36 -0500

    Delete backend directory

--- backend/src/main/java/com/project/r2system/User/Role.java
+++
@@ -1,6 +0,0 @@
-package com.project.r2system.User;
-
-public enum Role {
-    ADMIN,
-    USER  
-}
--- backend/src/main/java/com/project/r2system/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.r2system.User;
-
-import java.util.Optional;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,Integer> {
-    Optional<User> findByUsername(String username); 
-}
